556203b43af6b69bde5ee622238cef50f63f5e32
Yanortiz <157431552+Yanortiz@users.noreply.github.com>
2024-01-23 21:43:25 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+calificacion_numerica = 
+float (input("ingresa la calificacion numerica"))
+if calificacion_numerica >= 90:
+    calificacion_letra = "A"
+    if calificacion_numerica >= 80
+    calificacion_letra = "B"
+    if calificacion_numerica >= 70
+    calificacion_letra = "C"
+    if calificacion_numerica >= 60
+    calificacion_letra = "D"
+else:
+    calificacion_letra = "F"
+    print ("tu calificacion en letras es:"calificacion_letra)
