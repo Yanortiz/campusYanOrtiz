356f217165d7d3ec4a4cd614bf5db0bf1a1c2001
Yanortiz <157431552+Yanortiz@users.noreply.github.com>
2024-01-23 22:01:27 -0500

    Create ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,6 @@
+contrasena_predefinida = "miContrasena" 
+contrasena_ingresada = input("Ingrese su contraseña: ") 
+if contrasena_ingresada == contrasena_predefinida: 
+    print("Acceso concedido") 
+else: 
+    print("Contraseña incorrecta. Acceso denegado")
